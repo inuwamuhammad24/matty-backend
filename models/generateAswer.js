2d2852ffc5f04fde04cf157213b38f61574fbc52
inuwamuhammad24 <inuwamuhammad24@gmail.com>
2025-11-01 01:40:30 +0100

    Change model to back t zai-org

--- models/generateAswer.js
+++ models/generateAswer.js
@@ -4,7 +4,7 @@ dotenv.config()
 
 const client = new OpenAI({
   baseURL: "https://router.huggingface.co/v1",
-  apiKey: process.env.HUGGINGFACEACCESSTOKEN1,
+  apiKey: process.env.HUGGINGFACEACCESSTOKEN,
 })
 
 exports.genAnswer = (question, context) => {
@@ -12,10 +12,10 @@ exports.genAnswer = (question, context) => {
     const prompt = `You are a helpful and knowledgeable assistant known as Matty. Student Interact with you as their chatbot for information About University of Jos.
     I want you to chat with student just like an intelligent human. Each user's chat will be associated with a context. If a students chat is a question, use the context to answer it, else, chat with the student as a normal chatbot.
     Please take note of when the student's Chat is not a question and respond accordingly without regard to the context
-    Your responses should be in paragraphs and unordered list formatted in Markdown`
+    Your responses should be in paragraphs, ordered and unordered list formatted in Markdown`
     try {
       const output = await client.chat.completions.create({
-        model: "google/gemma-2-2b-it",
+        model: "zai-org/GLM-4.6:novita",
         messages: [{ role: "system", content: prompt }].concat(question),
       })
       resolve(output.choices[0].message.content)
